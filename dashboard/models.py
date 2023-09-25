c2fb8b7e9eb5a6d21e338e90bedaf3114d163c63
Peter Martin <petermartin602@gmail.com>
2023-09-25 21:09:25 +0300

    update on min value for name fields to be 3

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,18 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MinLengthValidator
 
 class ServiceProviderPersonalInfo(models.Model):
-    first_name=models.CharField(max_length=50, null=False)
-    last_name=models.CharField(max_length=50, null=False)
+    first_name = models.CharField(
+        max_length=50,
+        null=False,
+        validators=[MinLengthValidator(limit_value=3, message="First name must be at least 3 characters.")]
+    )
+
+    last_name = models.CharField(
+        max_length=50,
+        null=False,
+        validators=[MinLengthValidator(limit_value=3, message="Last name must be at least 3 characters.")]
+    )
     email=models.EmailField(max_length=100, null=False )
     phone_number = models.CharField(max_length=50, default='(+254) 000-0000', null=False)
 
-    
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
     
 class ServiceProviderWorkInfo(models.Model):
-    personal_info = models.OneToOneField(ServiceProviderPersonalInfo, on_delete=models.CASCADE, primary_key=True)
+    personal_info = models.OneToOneField(ServiceProviderPersonalInfo, on_delete=models.CASCADE, primary_key=True, default=None)
     profession=models.CharField(max_length=50, null=False)
     location=models.CharField(max_length=50, null=False)
     years_of_experince= models.IntegerField(default=0, null=False)
