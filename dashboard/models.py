f1ebc97ae239c9d5efc7e6b8d8f7ab82cd2d7383
Peter Martin <petermartin602@gmail.com>
2023-09-25 13:42:09 +0300

    Update for serviceproviderworkinfo to inherit personal info from serviceproviderpersonalinfo

--- dashboard/models.py
+++ dashboard/models.py
@@ -9,9 +9,10 @@ class ServiceProviderPersonalInfo(models.Model):
 
     
     def __str__(self):
-        return self.first_name
+        return f"{self.first_name} {self.last_name}"
     
 class ServiceProviderWorkInfo(models.Model):
+    personal_info = models.OneToOneField(ServiceProviderPersonalInfo, on_delete=models.CASCADE, primary_key=True)
     profession=models.CharField(max_length=50, null=False)
     location=models.CharField(max_length=50, null=False)
     years_of_experince= models.IntegerField(default=0, null=False)
