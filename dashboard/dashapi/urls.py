eca9ddd1100c1dc1f9e138869bbdd179d56fc829
Peter Martin <petermartin602@gmail.com>
2023-09-27 13:53:11 +0300

    update on work info url and also testcases for the views

--- dashboard/dashapi/urls.py
+++ dashboard/dashapi/urls.py
@@ -3,6 +3,7 @@ from dashboard.dashapi.views import ServiceProviderPersonalInfoAV, ServiceProvid
 
 urlpatterns = [
     path('personalinfo/', ServiceProviderPersonalInfoAV.as_view(), name='personal-info'),
-    path('personalinfo/<int:pk>/', ServiceProviderPersonalInfoAV.as_view(), name='personal-info'),
+    path('personalinfo/<int:pk>/', ServiceProviderPersonalInfoAV.as_view(), name='personal-info-detail'),
     path('workinfo/', ServiceProviderWorkInfoAV.as_view(), name='work-info'),
+    path('workinfo/<int:pk>/', ServiceProviderWorkInfoAV.as_view(), name='work-info-detail'),
 ]
--- dashboard/tests.py
+++ dashboard/tests.py
@@ -1,3 +1,61 @@
-from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your tests here.
+from rest_framework import status
+from rest_framework.test import APITestCase
+from rest_framework.authtoken.models import Token
+
+from dashboard.dashapi.serializer import serializers
+from dashboard import models
+
+class ServiceProviderPersonalInfoTestCase(APITestCase):
+    
+    def setUp(self):
+        self.personal_info = models.ServiceProviderPersonalInfo.objects.create(first_name='peter', last_name='martin', email='martin@gmail.com', phone_number='740190783')
+    
+    def test_serviceproviderpersonalinfo_create(self):
+        data = {
+            'first_name' : 'Peter',
+            'last_name' : 'Martin',
+            'email' : 'peter@gmail.com',
+            'phone_number' : '740190783'
+        }
+        response = self.client.post(reverse('personal-info'), data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+    
+    def test_serviceproviderpersonalinfo_update(self):
+        data={
+            'first_name' : 'Peter',
+            'last_name' : 'Kirika',
+            'email' : 'martin@gmail.com',
+            'phone_number' : '740190783',
+        }
+        response = self.client.put(reverse('personal-info-detail', args=(self.personal_info.id,)), data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+class ServiceProviderWorkInfoTestCase(APITestCase):
+    
+    def setUp(self):
+        self.personal_info = models.ServiceProviderPersonalInfo.objects.create(first_name='peter', last_name='martin', email='martin@gmail.com', phone_number='740190783')
+        
+        self.work_info = models.ServiceProviderWorkInfo.objects.create(personal_info=self.personal_info, profession='Plumber', location='juja', years_of_experience=4)
+    
+    def test_serviceproviderworkinfo_create(self):
+        data = {
+            'personal_info' : 1,
+            'profession' : 'plumber',
+            'location' : 'juja',
+            'years_of_experience' : 4
+        }
+        response = self.client.post(reverse('work-info'), data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_serviceproviderworkinfo_update(self):
+        data = {
+            'personal_info' : self.personal_info.id,
+            'profession' : 'Electrician',
+            'location' : 'juja',
+            'years_of_experience' : 4
+        }
+        response = self.client.put(reverse('work-info-detail', args=(self.work_info.pk,)), data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
