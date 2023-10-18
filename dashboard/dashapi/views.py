a075866b64c41873c03bb447c066487311c9de6d
publica <petermartin602@gmail.com>
2023-10-18 13:29:56 +0300

    Admindashboard

--- dashboard/dashapi/views.py
+++ dashboard/dashapi/views.py
@@ -6,51 +6,65 @@ from rest_framework import status
 from dashboard.dashapi.serializer import ServiceProviderPesrsonalInfoSerializer, ServiceProviderWorkInfoSerializer
 from dashboard.models import ServiceProviderPersonalInfo, ServiceProviderWorkInfo
 # from dashboard.dashapi.permissions import IsAdminOrReadOnly
-class ServiceProviderPersonalInfoAV(APIView):
-    # permission_classes=[IsAdminOrReadOnly]
+# class ServiceProviderPersonalInfoAV(APIView):
+#     # permission_classes=[IsAdminOrReadOnly]
     
-    def post(self, request):
-        serializer = ServiceProviderPesrsonalInfoSerializer(data = request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        else:
-            return Response(serializer.errors) 
+#     def post(self, request):
+#         serializer = ServiceProviderPesrsonalInfoSerializer(data = request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         else:
+#             return Response(serializer.errors) 
         
-    def put(self, request, pk):
-        try:
-            personal_info = ServiceProviderPersonalInfo.objects.get(pk = pk)
-        except ServiceProviderPersonalInfo.DoesNotExist: 
-            return Response({'Error' : 'INFO NOT FOUND'}, status=status.HTTP_404_NOT_FOUND) 
+#     def put(self, request, pk):
+#         try:
+#             personal_info = ServiceProviderPersonalInfo.objects.get(pk = pk)
+#         except ServiceProviderPersonalInfo.DoesNotExist: 
+#             return Response({'Error' : 'INFO NOT FOUND'}, status=status.HTTP_404_NOT_FOUND) 
           
-        serializer = ServiceProviderPesrsonalInfoSerializer(personal_info, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#         serializer = ServiceProviderPesrsonalInfoSerializer(personal_info, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         else:
+#             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         
-class ServiceProviderWorkInfoAV(APIView):
-    # permission_classes=[IsAdminOrReadOnly]
+# class ServiceProviderWorkInfoAV(APIView):
+#     # permission_classes=[IsAdminOrReadOnly]
     
-    def post(self, request):
-        serializer = ServiceProviderWorkInfoSerializer(data = request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        else:
-            return Response(serializer.errors) 
+#     def post(self, request):
+#         serializer = ServiceProviderWorkInfoSerializer(data = request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         else:
+#             return Response(serializer.errors) 
         
-    def put(self, request, pk):
-        try:
-            personal_info = ServiceProviderWorkInfo.objects.get(pk = pk)
-        except ServiceProviderWorkInfo.DoesNotExist: 
-            return Response({'Error' : 'INFO NOT FOUND'}, status=status.HTTP_404_NOT_FOUND) 
+#     def put(self, request, pk):
+#         try:
+#             personal_info = ServiceProviderWorkInfo.objects.get(pk = pk)
+#         except ServiceProviderWorkInfo.DoesNotExist: 
+#             return Response({'Error' : 'INFO NOT FOUND'}, status=status.HTTP_404_NOT_FOUND) 
           
-        serializer = ServiceProviderWorkInfoSerializer(personal_info, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#         serializer = ServiceProviderWorkInfoSerializer(personal_info, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         else:
+#             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class Admindashboard(APIView):
+    
+    def get(self,request):
+        personal_info = ServiceProviderWorkInfo.objects.all()
+        serializer = ServiceProviderWorkInfoSerializer(personal_info, many=True)
+        return Response(serializer.data)
+    
+    def put(self, request, pk):
+        
+        provider = ServiceProviderWorkInfo.objects.get(id=pk.id)
+        provider.status = not provider.status
+        provider.save()
+        return Response({'message': 'Status changed'})
         
