b63aa42ce6e02f6d206b17f5bdef637606b29be1
publica <petermartin602@gmail.com>
2023-10-18 18:46:57 +0300

    Url for Admindashboard

--- dashboard/dashapi/urls.py
+++ dashboard/dashapi/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from dashboard.dashapi.views import ServiceProviderPersonalInfoAV, ServiceProviderWorkInfoAV
+from dashboard.dashapi.views import Admindashboard, ServiceProviderPersonalInfoAV, ServiceProviderWorkInfoAV
 
 urlpatterns = [
-    path('personalinfo/', ServiceProviderPersonalInfoAV.as_view(), name='personal-info'),
-    path('personalinfo/<int:pk>/', ServiceProviderPersonalInfoAV.as_view(), name='personal-info-detail'),
-    path('workinfo/', ServiceProviderWorkInfoAV.as_view(), name='work-info'),
-    path('workinfo/<int:pk>/', ServiceProviderWorkInfoAV.as_view(), name='work-info-detail'),
+    # path('personalinfo/', ServiceProviderPersonalInfoAV.as_view(), name='personal-info'),
+    # path('personalinfo/<int:pk>/', ServiceProviderPersonalInfoAV.as_view(), name='personal-info-detail'),
+    # path('workinfo/', ServiceProviderWorkInfoAV.as_view(), name='work-info'),
+    # path('workinfo/<int:pk>/', ServiceProviderWorkInfoAV.as_view(), name='work-info-detail'),
+    path('admindashboard/', Admindashboard.as_view(), name='admind-dashboard'),
+    path('admindashboard/<int:pk>/', Admindashboard.as_view(), name='admind-dashboard-toggled'),
 ]
