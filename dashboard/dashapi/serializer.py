9edf773eb5e1c3f9d381e5cb14a13fee5ad38cc5
publica <petermartin602@gmail.com>
2023-10-19 12:07:33 +0300

    excluded created and update field in serializer

--- dashboard/dashapi/serializer.py
+++ dashboard/dashapi/serializer.py
@@ -5,11 +5,20 @@ class ServiceProviderPesrsonalInfoSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = ServiceProviderPersonalInfo
-        fields = '__all__'
+        exclude = ['created', 'Update']
         
 class ServiceProviderWorkInfoSerializer(serializers.ModelSerializer):
-    
+    personal_info = serializers.SerializerMethodField()
+
     class Meta:
         model = ServiceProviderWorkInfo
         fields = '__all__'
+
+    def get_personal_info(self, obj):
         
+        personal_info = obj.personal_info
+        if personal_info:
+            personal_info_serializer = ServiceProviderPesrsonalInfoSerializer(personal_info)
+            return personal_info_serializer.data
+        else:
+            return None
